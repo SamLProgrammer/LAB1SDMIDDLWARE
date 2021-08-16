e16ea626415171988fd8fc89330d11b284e0ccbb
Samuel López <samuel.lopez@uptc.edu.co>
2021-08-15 20:56:20 -0500

    First Push To Middleware

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 4000
+const shell = require('shelljs')
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+ shell.exec('./home/debian2/pinger.sh')
+
