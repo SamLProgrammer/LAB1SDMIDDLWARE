df376f39fae6ba9baf4853ddaf9131415b4b5505
Samuel López <samuel.lopez@uptc.edu.co>
2021-08-19 11:27:32 -0500

    trying to read logger file

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ app.listen(port, () => {
 
 function yourFunction(){
   shell.exec('/home/debian2/pinger.sh')
+  let fs  = require("fs");
+  let array = fs.readFileSync('/home/debian2/last_debian0_ping.txt').toString().split('\n');
+  console.log(array[0])
   setTimeout(yourFunction, 2000);
 }
 
