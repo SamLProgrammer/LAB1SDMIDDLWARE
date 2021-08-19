9d66306087f3307b1615038decbb7b1f935140a2
Samuel López <samuel.lopez@uptc.edu.co>
2021-08-19 17:33:30 -0500

    getting only httpcode

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express')
 const app = express()
 const port = 4000
 const shell = require('shelljs')
+let server1State = '';
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 app.use(express.static('./views'));
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {data: server1State})
 })
 
 app.listen(port, () => {
@@ -19,7 +20,7 @@ function yourFunction(){
   shell.exec('/home/debian2/pinger.sh')
   let fs  = require("fs");
   let array = fs.readFileSync('/home/debian2/last_debian0_ping.txt').toString().split('\n');
-  console.log(array[0])
+  console.log(array[0].split(' ')[0])
   setTimeout(yourFunction, 2000);
 }
 
