14eeeff911d9d280f559c8fdfd846c403e0fd5e3
SamLProgrammer <samuel.lopez@uptc.edu.co>
2021-08-18 20:52:58 -0500

    adding date record to pinger.sh result

--- pinger.sh
+++ pinger.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
+current_date=$(date)
 last_debian0_ping=$(curl -s -o /dev/null -w '%{http_code}' http://192.168.100.12:4000/status)
 last_debian1_ping=$(curl -s -o /dev/null -w '%{http_code}' http://192.168.100.16:4000/status)
 history_debian0_ping=$(curl -s -o /dev/null -w '%{http_code}' http://192.168.100.12:4000/status)
 history_debian1_ping=$(curl -s -o /dev/null -w '%{http_code}' http://192.168.100.16:4000/status)
 
-
-echo "$last_debian0_ping" > /home/debian2/last_debian0_ping.txt
-echo "$last_debian1_ping" > /home/debian2/last_debian1_ping.txt
-echo "$history_debian0_ping" >> /home/debian2/history_debian0_ping.txt
-echo "$history_debian1_ping" >> /home/debian2/history_debian1_ping.txt
+echo "$last_debian0_ping $current_date" > /home/debian2/last_debian0_ping.txt
+echo "$last_debian1_ping $current_date" > /home/debian2/last_debian1_ping.txt
+echo "$history_debian0_ping $current_date" >> /home/debian2/history_debian0_ping.txt
+echo "$history_debian1_ping $current_date" >> /home/debian2/history_debian1_ping.txt
 
