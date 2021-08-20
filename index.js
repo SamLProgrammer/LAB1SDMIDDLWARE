daae17834ebc6c3a01359c2545255ef923918386
Samuel López <samuel.lopez@uptc.edu.co>
2021-08-19 22:07:05 -0500

    starting gui

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ app.listen(port, () => {
 function yourFunction(){
   shell.exec('/home/debian2/pinger.sh')
   let fs  = require("fs");
-  let array = fs.readFileSync('/home/debian2/last_debian0_ping.txt').toString().split('\n');
-  server1State = array[0].split(' ')[0];
-  let array = fs.readFileSync('/home/debian2/last_debian1_ping.txt').toString().split('\n');
-  server2State = array[0].split(' ')[0];
+  let array1 = fs.readFileSync('/home/debian2/last_debian0_ping.txt').toString().split('\n');
+  server1State = array1[0].split(' ')[0];
+  let array2 = fs.readFileSync('/home/debian2/last_debian1_ping.txt').toString().split('\n');
+  server2State = array2[0].split(' ')[0];
   setTimeout(yourFunction, 2000);
 }
 
