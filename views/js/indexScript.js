420ff011af02bab694cd150e6343fe95cadaf2f1
Samuel López <samuel.lopez@uptc.edu.co>
2021-08-19 21:50:40 -0500

    starting gui

--- views/js/indexScript.js
+++ views/js/indexScript.js
@@ -1,7 +1,7 @@
 window.onload = function () {
     const server1Button = document.getElementById('server1-button')
     const server2Button = document.getElementById('server2-button')
-    const serverRefreshButton = document.getElementById('refreshButton')
+    const serverRefreshButton = document.getElementById('refresh-button')
     serverRefreshButton.addEventListener('click', function (event) {requestUpdate(server1Button, server2Button)})
 }
 
@@ -10,7 +10,7 @@ function requestUpdate(button1, button2) {
     xhr.open("GET", 'http://192.168.100.10:4000/refresh', true);
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            const resp = JSON.stringify(xhr.response);
+            const resp = JSON.parse(xhr.response);
             alert(resp);
         }
     }
